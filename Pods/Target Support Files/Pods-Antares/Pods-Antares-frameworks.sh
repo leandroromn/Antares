0e6fd7c2b686d33407d803ccf85184f675c9030b
Leandro Romano <leandroromano.dev@gmail.com>
2019-05-25 23:00:47 -0300

    Implementation for Xcode configuration files.

--- Pods/Target Support Files/Pods-Antares/Pods-Antares-frameworks.sh
+++ Pods/Target Support Files/Pods-Antares/Pods-Antares-frameworks.sh
@@ -156,6 +156,10 @@ if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/PromiseKit/PromiseKit.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/R.swift.Library/Rswift.framework"
 fi
+if [[ "$CONFIGURATION" == "Development" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/PromiseKit/PromiseKit.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/R.swift.Library/Rswift.framework"
+fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/PromiseKit/PromiseKit.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/R.swift.Library/Rswift.framework"
