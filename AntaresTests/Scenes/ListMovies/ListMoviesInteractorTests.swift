4da65558f5e9469c78dc13948a215a5e7405f58f
Leandro Romano <leandroromano.dev@gmail.com>
2019-06-01 20:15:10 -0300

    Removed AntaresTest default file and added ListMovieInteractorTests with the first success case of get movies.

---
+++ AntaresTests/Scenes/ListMovies/ListMoviesInteractorTests.swift
@@ -0,0 +1,56 @@
+//
+//  ListMoviesInteractorTests.swift
+//  AntaresTests
+//
+//  Created by Leandro Romano on 01/06/19.
+//  Copyright © 2019 Leandro Romano. All rights reserved.
+//
+
+import XCTest
+import Cuckoo
+import Nimble
+import PromiseKit
+
+@testable import Antares
+
+class ListMoviesInteractorTests: XCTestCase {
+
+    var sut: ListMoviesInteractor!
+    var worker: MockListMoviesWorker!
+    
+    override func setUp() {
+        super.setUp()
+        PromiseKit.conf.Q.map = nil
+        PromiseKit.conf.Q.return = nil
+        
+        worker = MockListMoviesWorker()
+        sut = ListMoviesInteractor(worker: worker)
+    }
+    
+    func testGetPopularMoviesSuccess() {
+        let mockPresenter = MockListMoviesPresenter()
+        
+        stub(mockPresenter) { stub in
+            when(stub.presentMovies(response: any())).thenDoNothing()
+        }
+        
+        stub(worker) { stub in
+            when(stub.retrieveMovies(for: any())).then { request -> Promise<ListMovies.Response> in
+                let response = ListMovies.Response(page: 1, totalPages: 100, totalResults: 1000, results: [])
+                return .value(response)
+            }
+        }
+        
+        sut.presenter = mockPresenter
+        sut.getMovies(by: .popular)
+        
+        verify(mockPresenter, times(1)).presentMovies(response: any())
+    }
+
+    override func tearDown() {
+        super.tearDown()
+        worker = nil
+        sut = nil
+    }
+
+}
